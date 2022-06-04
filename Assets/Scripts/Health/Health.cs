b989f85b9ed37227421776f1ea67859465ff38d8
Marci <lm4459533@gmail.com>
2022-06-04 17:57:49 +0200

    dash added

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour, IHpManager
+public class Health : MonoBehaviour
 {
     public int SetHealth { protected set; get; }
     [SerializeField] protected GameObject particles;
--- Assets/Scripts/Mozgas/MozgasPlayer.cs
+++ Assets/Scripts/Mozgas/MozgasPlayer.cs
@@ -6,14 +6,62 @@ public class MozgasPlayer : Mozgas
 {
     private float horizontalis;
     private float vertikalis;
+
+    private float dashCoolDown = 0;
+    private float dash = 23;
+    private float length = 0.3f;
+
+    private float speedActive;
+
+    private bool dashing = false;
+
+
+    private void Start()
+    {
+        entity = GetComponent<Rigidbody2D>();
+        speedActive = speed;
+    }
     private void Update()
     {
         horizontalis = Input.GetAxisRaw("Horizontal");
         vertikalis = Input.GetAxisRaw("Vertical");
+
+        CheckForDash();
     }
 
     private void FixedUpdate()
     {
-        entity.velocity = (new Vector2(horizontalis, vertikalis) * speed);
+        entity.velocity = (new Vector2(horizontalis, vertikalis).normalized * speedActive);
+
+        Dash();
+    }
+
+
+    private void CheckForDash()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dashing = true;
+        }
+    }
+    private void Dash()
+    {
+        if (dashing && dashCoolDown <= Time.time && length <= 0)
+        {
+            length = 0.3f;
+            speedActive = dash;
+            dashCoolDown = Time.time + 3;
+        }
+
+        if (length > 0)
+        {
+            length -= Time.deltaTime;
+
+            if (length <= 0)
+            {
+                speedActive = speed;
+            }
+        }
+        dashing = false;
     }
 }
