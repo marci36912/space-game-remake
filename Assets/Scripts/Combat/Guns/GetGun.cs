ccfb1dd495ee42012af532044957045d32471a37
Marci <lm4459533@gmail.com>
2022-06-13 14:20:30 +0200

    inventory works shooting doesnt

--- Assets/Scripts/Combat/Guns/GetGun.cs
+++ Assets/Scripts/Combat/Guns/GetGun.cs
@@ -6,7 +6,12 @@ public class GetGun : MonoBehaviour
 {
     [SerializeField] private List<GameObject> Guns;
     private GameObject activeGun;
- 
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GetComponentInParent<Inventory>();
+    }
     public void SetGun(int n)
     {
         if (transform.childCount != 0)
@@ -15,6 +20,7 @@ public class GetGun : MonoBehaviour
         }
 
         activeGun = Instantiate(Guns[n], transform);
+        inventory.setSelected(Guns[n], activeGun, GunStats.ReturnGun(n));
         activeGun.GetComponent<Shooting>().setStats(GunStats.ReturnGun(n));
     }
 }
--- Assets/Scripts/Combat/Inventory.cs
+++ Assets/Scripts/Combat/Inventory.cs
@@ -7,26 +7,47 @@ public class Inventory : MonoBehaviour
 {
     [SerializeField] private Image[] selectedPics;
 
+    private GameObject activeGun;
+
     private GameObject[] guns = new GameObject[2];
     private int selected = 0;
 
     private int tmp = 0;
 
+    private Transform gp;
+
+    private void Start()
+    {
+        gp = transform.Find("rotation").transform.Find("GunPoint");
+    }
+
     void Update()
     {
         selectSlot();
-        selectedColor();
         selectedGun();
     }
-
+    
+    public void setSelected(GameObject gun, GameObject active, Gun stats)
+    {
+        guns[selected] = gun;
+        guns[selected].GetComponent<Shooting>().setStats(stats);
+        selectedPics[selected].sprite = gun.GetComponent<SpriteRenderer>().sprite;
+        activeGun = active;
+    }
     private void selectedGun()
     {
         if (tmp == selected) return;
         else
         {
-            //fegyvervaltas
-            Debug.Log("fasz");
             tmp = selected;
+
+            if (gp.childCount != 0) Destroy(activeGun);
+            
+            Debug.Log(guns[selected] == null ? "null" : guns[selected].name);
+
+            if (guns[selected] != null) activeGun = Instantiate(guns[selected], gp);
+            
+            selectedColor();
         }
     }
     private void selectSlot()
