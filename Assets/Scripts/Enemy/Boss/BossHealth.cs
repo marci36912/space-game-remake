f632fdbffff7f3b9ed1bc46a70104ee5b264aae5
Marci <lm4459533@gmail.com>
2022-07-05 21:47:11 +0200

    boss stages

--- Assets/Scripts/Enemy/Boss/BossHealth.cs
+++ Assets/Scripts/Enemy/Boss/BossHealth.cs
@@ -7,8 +7,18 @@ public class BossHealth : Health, IHpManager
 {
     [SerializeField] private GameObject teleport;
 
+    [SerializeField] private GameObject shield;
+    [SerializeField] private GameObject flyers;
+    [SerializeField] private GameObject smoke;
+
+    [SerializeField] private Sprite[] stages;
+
     [SerializeField] private Slider healthBar;
     [SerializeField] private int MaxHealth;
+
+    private bool secondStage = false;
+    private bool thirdStage = false;
+
 
     private void Start()
     {
@@ -17,10 +27,31 @@ public class BossHealth : Health, IHpManager
         healthBar.value = SetHealth;
     }
 
+    private void stageCheck()
+    {
+        if (SetHealth / MaxHealth >= 0.5 && !secondStage)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = stages[0];
+            secondStage = true;
+            shield.SetActive(true);
+        }
+        else if (SetHealth / MaxHealth >= 0.15 && !thirdStage)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = stages[1];
+            thirdStage = true;
+            smoke.SetActive(true);
+            for (int i = 0; i < 3; i++)
+            {
+                Instantiate(flyers, transform.position, Quaternion.identity);
+            }
+        }
+    }
+
     public override void getDamage(int n)
     {
         base.getDamage(n);
         healthBar.value = SetHealth;
+        stageCheck();
     }
 
     public override void Death()
